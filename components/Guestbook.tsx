90d94f8b4222bb403053cb620bc57deb9fcee1d6
Nguyễn Phương Nam <119719970+npnamdev@users.noreply.github.com>
2025-03-01 14:29:06 +0000

    update 4

--- components/Guestbook.tsx
+++ components/Guestbook.tsx
@@ -4,19 +4,15 @@ import { useState } from "react";
 import { Input } from "./ui/input";
 import { Textarea } from "./ui/textarea";
 import { Button } from "./ui/button";
+import { toast } from "sonner";
 
 const Guestbook: React.FC = () => {
   const [name, setName] = useState("");
   const [phone, setPhone] = useState("");
   const [message, setMessage] = useState("");
-  const [submitted, setSubmitted] = useState(false);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState("");
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
-    setLoading(true);
-    setError("");
 
     try {
       const response = await fetch("https://weddingserver-1.onrender.com/wishes", {
@@ -32,17 +28,20 @@ const Guestbook: React.FC = () => {
       }
 
       console.log("Lời chúc đã gửi:", { name, phone, message });
-      setSubmitted(true);
-      setTimeout(() => setSubmitted(false), 3000);
+      toast("Gửi lời chúc thành công!", {
+        description: "Cảm ơn bạn đã đồng hành cùng chúng tôi!",
+        action: {
+          label: "Đóng",
+          onClick: () => console.log("Đóng thông báo"),
+        },
+      });
 
       setName("");
       setPhone("");
       setMessage("");
     } catch (err: any) {
       setError(err.message);
-    } finally {
-      setLoading(false);
-    }
+    } 
   };
 
   return (
@@ -57,10 +56,8 @@ const Guestbook: React.FC = () => {
           <Input type="text" value={name} onChange={(e) => setName(e.target.value)} placeholder="Nhập tên của bạn" className="bg-white py-5 text-[15px]" required />
           <Input type="tel" value={phone} onChange={(e) => setPhone(e.target.value)} placeholder="Nhập số điện thoại của bạn" className="bg-white py-5 text-[15px]" required />
           <Textarea value={message} onChange={(e) => setMessage(e.target.value)} placeholder="Nhập lời chúc của bạn" className="bg-white py-2.5 h-[120px] text-[15px]" required />
-          <Button variant="outline" type="submit" disabled={loading} className="py-6 bg-white text-black fon-bold uppercase rounded-xl w-full mt-1 text-[14px]">{loading ? "Đang gửi..." : "Gửi lời chúc"}</Button>
+          <Button variant="outline" type="submit" disabled={loading} className="py-6 bg-white text-black fon-bold uppercase rounded-xl w-full mt-1 text-[14px]">Gửi lời chúc</Button>
         </form>
-        {submitted && <p className="text-green-600 mb-4">Cảm ơn bạn đã gửi lời chúc!</p>}
-        {error && <p className="text-red-600 mb-4">{error}</p>}
       </div>
     </section>
   );
