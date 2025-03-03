946ccf6b616aeed397993d2cf62fd056fcf89be7
Nguyễn Phương Nam <119719970+npnamdev@users.noreply.github.com>
2025-03-03 17:03:18 +0700

    update width comment

--- components/CommentWish.tsx
+++ components/CommentWish.tsx
@@ -1,6 +1,7 @@
 "use client";
 
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { Button } from "@/components/ui/button";
 
 interface Wish {
   _id: string;
@@ -11,13 +12,24 @@ interface Wish {
 
 interface Props {
   wishes: Wish[];
-  fetchWishes: () => void; 
+  fetchWishes: () => void;
 }
 
 const CommentWish: React.FC<Props> = ({ wishes, fetchWishes }) => {
+  const [visibleWishes, setVisibleWishes] = useState<Wish[]>([]);
+  const [count, setCount] = useState(3);
+
   useEffect(() => {
     fetchWishes();
   }, []);
+
+  useEffect(() => {
+    setVisibleWishes(wishes.slice(-count));
+  }, [wishes, count]);
+
+  const handleLoadMore = () => {
+    setCount((prev) => Math.min(prev + 3, wishes.length));
+  };
 
   return (
     <div className="p-6 bg-primary shadow-md mt-2 rounded-md">
@@ -27,12 +39,20 @@ const CommentWish: React.FC<Props> = ({ wishes, fetchWishes }) => {
         </div>
       ) : (
         <div className="space-y-2">
-          {wishes.map((wish) => (
+          {visibleWishes.map((wish) => (
             <div key={wish._id} className="px-4 py-3 border rounded-lg shadow-sm bg-gray-50">
               <h3 className="font-semibold text-sm text-gray-900 uppercase">{wish.name}</h3>
-              <p className="text-gray-700 mt-1 text-sm upa">{wish.message}</p>
+              <p className="text-gray-700 mt-1 text-sm">{wish.message}</p>
             </div>
           ))}
+          {visibleWishes.length < wishes.length && (
+            <Button variant="outline"
+              onClick={handleLoadMore}
+              className="px-6"
+            >
+              Xem thêm lời chúc
+            </Button>
+          )}
         </div>
       )}
     </div>
