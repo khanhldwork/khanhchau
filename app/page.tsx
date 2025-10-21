37f09dcae0eb0e43553ecea56ea32427b9db3a2d
Nguyễn Phương Nam <119719970+npnamdev@users.noreply.github.com>
2025-10-21 09:41:43 +0700

    fix bug 3

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+"use client"
 
 import AOS from "aos";
 import "aos/dist/aos.css";
@@ -18,36 +19,11 @@ import { Section1 } from "@/components/component-anh-khanh/Section1";
 import Section2 from "@/components/component-anh-khanh/Section2";
 import Section3 from "@/components/component-anh-khanh/Section3";
 import DoorOpeningEffect from "@/components/DoorOpeningEffect";
+import { useEffect } from "react";
 
-export const metadata = {
-  title: "Thiệp mời cưới Phương Nam ♥ Hải Yến",
-  description:
-    "Trân trọng kính mời bạn đến chung vui cùng chúng tôi trong ngày trọng đại của Phương Nam và Hải Yến. Hãy cùng nhau tạo nên những khoảnh khắc ý nghĩa và đáng nhớ!",
-  openGraph: {
-    title: "Thiệp mời cưới Phương Nam ♥ Hải Yến",
-    description:
-      "Trân trọng kính mời bạn đến chung vui cùng chúng tôi trong ngày trọng đại của Phương Nam và Hải Yến. Hãy cùng nhau tạo nên những khoảnh khắc ý nghĩa và đáng nhớ!",
-    url: "https://wedly.info",
-    siteName: "Thiệp mời cưới Phương Nam ♥ Hải Yến",
-    images: [
-      "https://res.cloudinary.com/dpufemrnq/image/upload/v1740504936/257A6768_olhw6i.jpg",
-    ],
-    locale: "vi_VN",
-    type: "website",
-  },
-  twitter: {
-    card: "summary_large_image",
-    title: "Thiệp mời cưới Phương Nam ♥ Hải Yến",
-    description:
-      "Trân trọng kính mời bạn đến chung vui cùng chúng tôi trong ngày trọng đại của Phương Nam và Hải Yến.",
-    images: [
-      "https://res.cloudinary.com/dpufemrnq/image/upload/v1740504936/257A6768_olhw6i.jpg",
-    ],
-  },
-};
 
 export default function Page() {
-  // useEffect(() => { AOS.init(); }, []);
+  useEffect(() => { AOS.init(); }, []);
 
 
 
